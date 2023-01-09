b4c14e7dfbf17c735fbd40ca0709dadd0c316bea
Ricardo Setti <rsetti@msn.com>
2023-01-09 00:04:33 -0500

    Day 3 done

--- Day3/day3.py
+++ Day3/day3.py
@@ -1,6 +1,7 @@
 def intersection(lst1, lst2, lst3):
     return list(set(lst1) & set(lst2) & set(lst3))
 
+
 items = [0, "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r",
          "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
          "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
@@ -10,16 +11,16 @@ allBackpacks = []
 with open("day3.txt") as file:
     for line in file:
 
-         # initializing the string
-         backpack = line.strip()
-         allBackpacks.append(backpack)
-         print(backpack)
-         quantityItems = len(backpack)
-         sack1 = backpack[:quantityItems // 2]
-         sack2 = backpack[quantityItems // 2:]
-         # print(sack1, sack2)
-         found = False
-         for itemSack1 in sack1:
+        # initializing the string
+        backpack = line.strip()
+        allBackpacks.append(backpack)
+        print(backpack)
+        quantityItems = len(backpack)
+        sack1 = backpack[:quantityItems // 2]
+        sack2 = backpack[quantityItems // 2:]
+        # print(sack1, sack2)
+        found = False
+        for itemSack1 in sack1:
             if not found:
                 for i in range(0, len(sack2)):
                     if itemSack1 == sack2[i]:
@@ -34,11 +35,11 @@ with open("day3.txt") as file:
     print(totalPriorities)
     print(len(duplicate_char))
     badges = []
-    for i in range (0, len(allBackpacks), 3):
+    for i in range(0, len(allBackpacks), 3):
         group = []
         group.append(allBackpacks[i])
-        group.append(allBackpacks[i+1])
-        group.append(allBackpacks[i+2])
+        group.append(allBackpacks[i + 1])
+        group.append(allBackpacks[i + 2])
         print(group)
         common = intersection(group[0], group[1], group[2])
         badges.append(common[0])
